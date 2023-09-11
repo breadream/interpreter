55c41ee152bce16e6f73bc2c62e35eefce4872b3
Brad Lim <brad.dh.lim@gmail.com>
2023-09-11 15:43:28 -0700

    Snapshot commit; add ast and parser skeleton

---
+++ monkey/parser/parser.go
@@ -0,0 +1,21 @@
+package parser
+
+import (
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.NextToken()
+	p.NextToken()
+}
